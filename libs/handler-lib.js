3a3d4d0407de024f0b3345a37ac43f5c4a1d0417
Erik Blixt Dackhammar <blixtdackhammar@gmail.com>
2021-02-02 10:27:25 +0100

    Adding our serverless API

--- libs/handler-lib.js
+++ libs/handler-lib.js
@@ -7,7 +7,6 @@ export default function handler(lambda) {
             body = await lambda(event, context);
             statusCode = 200;
         } catch (e) {
-            console.log(e);
             body = { error: e.message };
             statusCode = 500;
         }
